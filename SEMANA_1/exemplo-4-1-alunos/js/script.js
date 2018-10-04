fdf603248937f38571e787eac455261765f4924b
mariliamessias <marilia.oliveira@outlook.com>
2018-10-03 22:17:33 -0300

    Aula 03 Javascript

--- SEMANA_1/exemplo-4-1-alunos/js/script.js
+++ SEMANA_1/exemplo-4-1-alunos/js/script.js
@@ -1,4 +1,17 @@
-const aluno = document.querySelector("#primeiro-da-lista");
+const todosAlunos = document.querySelectorAll(".aluno"); // pega todos os elementos da classe aluno
 
-console.log(aluno);
+for (let i = 0; i < todosAlunos.length; i++)
+{
+    //console.log(todosAlunos[i]);
+    // que uma aluna, vai assumir a primeira posicao
+    const aluno = todosAlunos[i];
+    const tdAlunoNotaUm = aluno.querySelector(".info-notaum");
+    const primeiraNota = tdAlunoNotaUm.textContent;
+    const tdAlunoNotaDois = aluno.querySelector(".info-notadois");
+    const segundaNota = tdAlunoNotaDois.textContent;
+    const tdMedia = aluno.querySelector(".info-media");
 
+    const calculo = (parseFloat(primeiraNota) + parseFloat(segundaNota )) / 2;
+
+    tdMedia.textContent = calculo.toFixed(2);
+}
