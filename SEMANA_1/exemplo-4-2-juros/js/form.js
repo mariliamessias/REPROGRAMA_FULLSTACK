1970ff839f3463960b09aebd49f5f9dbb4f1ab98
mariliamessias <marilia.oliveira@outlook.com>
2018-10-08 13:47:59 -0300

    Aula 4

--- SEMANA_1/exemplo-4-2-juros/js/form.js
+++ SEMANA_1/exemplo-4-2-juros/js/form.js
@@ -14,18 +14,26 @@ botaoAdd.addEventListener('click', function(event){
     const taxaTd = document.createElement("td");
     const tempoTd = document.createElement("td");
     const resultadoTd = document.createElement("td");
+    
+    if (validaVazio(money.value, taxa.value, tempo.value)){
+        
+        if (validaNumero(money.value, taxa.value, tempo.value))
+        {
+            var juros = calculaJuros(money.value, taxa.value, tempo.value);
 
-    moneyTd.textContent = money.value;
-    taxaTd.textContent = taxa.value;
-    tempoTd.textContent = tempo.value;
-    resultadoTd.textContent = calculaJuros(money.value, taxa.value, tempo.value);
-
-    jurosTr.appendChild(moneyTd);
-    jurosTr.appendChild(taxaTd);
-    jurosTr.appendChild(tempoTd);
-    jurosTr.appendChild(resultadoTd);
-
-    const tabelaJuros = document.getElementById("tabela-juros");
-
-    tabelaJuros.appendChild(jurosTr);
+            moneyTd.textContent = money.value;
+            taxaTd.textContent = taxa.value;
+            tempoTd.textContent = tempo.value;
+            resultadoTd.textContent = juros;
+    
+            jurosTr.appendChild(moneyTd);
+            jurosTr.appendChild(taxaTd);
+            jurosTr.appendChild(tempoTd);
+            jurosTr.appendChild(resultadoTd);
+    
+            const tabelaJuros = document.getElementById("tabela-juros");
+    
+            tabelaJuros.appendChild(jurosTr);
+        }
+    }    
 });
--- SEMANA_1/exemplo-4-2-juros/js/juros.js
+++ SEMANA_1/exemplo-4-2-juros/js/juros.js
@@ -16,7 +16,6 @@ for( let i = 0; i < dinheiro.length; i++)
     const infoResultado = dinheiroSelecionado.querySelector(".info-resultado");
    
     infoResultado.textContent = calculaJuros(infoTaxaValor, infoDinheiroValor,infoTempoValor );
-   
 }
 
 function calculaJuros(taxa, valor, tempo)
@@ -24,4 +23,78 @@ function calculaJuros(taxa, valor, tempo)
     const calculo = parseFloat(taxa) * parseFloat(valor) * parseFloat(tempo);
 
    return calculo.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
+
+mensagem = document.getElementById("msgForm");
+
+function validaNumero(valor, valor1, valor2){
+    
+    mensagem.textContent = "";
+    
+   if (isNaN(valor) || isNaN(valor1) || isNaN(valor2)){
+    //caso seja número
+        mensagem.textContent = "informe apenas números!";
+        return false;
+    } else return true;  
+}
+
+function validaVazio(m, ta,te ){
+
+    money = document.getElementById("dinheiro");
+    taxa = document.getElementById("taxa");
+    tempo = document.getElementById("tempo");
+
+    if (m == "" && ta == "" & te == "")
+    {
+        money.style.borderColor = "red";
+        taxa.style.borderColor = "red";
+        tempo.style.borderColor = "red";
+        return false;
+    }
+    else if (m == "" && ta == "")
+    {   
+        money.style.borderColor = "red";
+        taxa.style.borderColor = "red";
+        tempo.style.borderColor = "#ccc";
+        return false;
+    }
+    else if (m == "" && te == "")
+    {   
+        money.style.borderColor = "red";
+        taxa.style.borderColor = "#ccc";
+        tempo.style.borderColor = "red";
+        return false;
+    }
+    else if (te == "" && ta == "")
+    {
+        money.style.borderColor = "#ccc";
+        taxa.style.borderColor = "red";
+        tempo.style.borderColor = "red";
+        return false;
+    }
+    else if (m == "")
+    {
+        money.style.borderColor = "red";
+        taxa.style.borderColor = "#ccc";
+        tempo.style.borderColor = "#ccc";
+    }
+    else if (te == "")
+    {
+        money.style.borderColor = "#ccc";
+        taxa.style.borderColor = "#ccc";
+        tempo.style.borderColor = "red";
+    }
+    else if (ta == "")
+    {
+        money.style.borderColor = "#ccc";
+        taxa.style.borderColor = "red";
+        tempo.style.borderColor = "#ccc"; 
+    }
+    else
+    {
+        money.style.borderColor = "#ccc";
+        taxa.style.borderColor = "#ccc";
+        tempo.style.borderColor = "#ccc";
+        return true;
+    }
 }
