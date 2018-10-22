a82d7159751e62958940d9a13de304a296e02f15
mariliamessias <marilia.oliveira@outlook.com>
2018-10-22 20:59:49 -0300

    Aula 01 - React

--- SEMANA_4/Projeto/scripts.js
+++ SEMANA_4/Projeto/scripts.js
@@ -1,1 +1,19 @@
+const linkCriarUmaConta = document.createElement('a');
+const linkFazerLogin = document.createElement('a');
+const formulario = document.createElement('form');
+const divisaoProjeto = document.getElementById("projeto");
 
+formulario.className = "formulario";
+
+linkCriarUmaConta.className = "link";
+linkCriarUmaConta.href = "/conta";
+linkCriarUmaConta.appendChild(document.createTextNode('Criar uma conta'));
+
+formulario.appendChild(linkCriarUmaConta);
+
+linkFazerLogin.className = "link";
+linkFazerLogin.href = "/login";
+linkFazerLogin.appendChild(document.createTextNode('Fazer Login'));
+formulario.appendChild(linkFazerLogin);
+
+divisaoProjeto.appendChild(formulario);
