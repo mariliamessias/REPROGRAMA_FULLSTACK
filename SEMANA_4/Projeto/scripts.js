ec6b694341097ac1a5e420ddac9fac6453d3559c
mariliamessias <marilia.oliveira@outlook.com>
2018-10-22 22:25:41 -0300

    Aula 01 - React

--- SEMANA_4/Projeto/scripts.js
+++ SEMANA_4/Projeto/scripts.js
@@ -1,6 +1,11 @@
-const linkCriarUmaConta = React.createElement("a", {className:"link", href:"/conta", children:'Criar uma Conta'});
-const linkFazerLogin = React.createElement("a", {className:"link", href:"/login", children:'Fazer login'});
-const formularioLogin = React.createElement("form", {children: [linkCriarUmaConta, linkFazerLogin]});
+const linkCriarUmaConta = <a class="link" href="/conta">Criar uma conta</a>
+const linkFazerLogin = <a class="link" href="/login">Fazer Login</a>
+const formularioLogin =
+(
+        <form>
+            {[linkCriarUmaConta,linkFazerLogin]}
+        </form>
+)
 
 const divisaoProjeto = document.getElementById("projeto");
 
