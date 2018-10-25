dcbd943c48d8eab8072d13b7eb1b39e5493709e2
mariliamessias <marilia.oliveira@outlook.com>
2018-10-25 20:19:02 -0300

    Aula 04 - React

--- SEMANA_4/Projeto/src/index.js
+++ SEMANA_4/Projeto/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Login from './paginas/Login/Login';
+import Conta from './paginas/Conta/Conta';
 import './index.css'
 
 
-const pagina = <Login />
+const pagina = <Conta />
 
 const divisaoProjeto = document.getElementById('projeto')
 
