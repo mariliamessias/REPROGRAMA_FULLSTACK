a335339b6b2c0562e42111cf1077114f58fc0cc4
mariliamessias <marilia.oliveira@outlook.com>
2018-11-07 22:23:50 -0300

    Aula 04 - Redux
    Construindo os postits

--- SEMANA_4/Projeto/src/paginas/Home/Home.js
+++ SEMANA_4/Projeto/src/paginas/Home/Home.js
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './Home.css'
 import {Redirect} from 'react-router-dom'
 import {connect } from 'react-redux'
+import carregando from './loading.svg'
 //recebe o usuário via props pois está chamando do store
-function Home(props) {
+
+class Home extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { carregando: true}
+  }
 
   //se o usuário for nulo, será direcionado ao login
-  if (!props.usuario){
-    return <Redirect to="/login/"/>
-  }
-  
-  return (
+  render(){
+    if (!this.props.usuario){
+      return <Redirect to="/login/"/>
+    }
+    return(
      //se não tem usuário
     <main className="home">
+      {this.state.carregando ? (
+        <img className="home__loading" src={carregando} alt="Carregando"/>
+      ) : (
+        <div>
+            Aqui listarei os postits amanhã
+        </div>
+      )}
     </main>
-  )
+    )}
 }
 
 export default connect((state)=>({ usuario:state.usuario }))(Home) // { chave do objeto, o ( é para que seja identificada a função
-// não precisa passar a segunda pois o sisem, ja entende que é nulo
+// não precisa passar a segunda pois o sistem, ja entende que é nulo
 
 
